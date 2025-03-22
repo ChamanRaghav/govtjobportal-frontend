07a5164010aef0fbfb7a5c33153c8f3daa2d9726
Chaman Raghav <chamanraghav61@gmail.com>
2025-03-22 22:11:34 +0530

    Added Clerk Auth - In Progress

--- src/app/components/Navbar.js
+++ src/app/components/Navbar.js
@@ -1,33 +1,52 @@
 import React from 'react'
+import {
+    //   ClerkProvider,
+    SignInButton,
+    SignUpButton,
+    SignedIn,
+    SignedOut,
+    UserButton,
+} from '@clerk/nextjs';
 
-function Navbar({filters = {}, setFilters = () => {}}) {
+function Navbar({ filters = {}, setFilters = () => { } }) {
     return (
-        <div className='flex justify-between py-4 px-6  blue-background shadow-md'>
+        <div className='flex justify-between py-4 px-6 blue-background shadow-md'>
             <div className='flex items-center justify-center h-full mr-4'>
-                <h2 className='text-xl font-bold text-white'>GovtJobPortal</h2>
+                <h2 className='text-xl font-bold text-white'>GovtJobsPortal</h2>
             </div>
-            <div className='w-full max-w-lg flex items-center gap-3 border border-gray-300 rounded p-3 shadow-sm bg-white focus-within:ring-2 focus-within:ring-blue-500'>
-                <svg
-                    xmlns='http://www.w3.org/2000/svg'
-                    fill='none'
-                    viewBox='0 0 24 24'
-                    strokeWidth={1.5}
-                    stroke='currentColor'
-                    className='w-6 h-6 text-gray-500'
-                >
-                    <path
-                        strokeLinecap='round'
-                        strokeLinejoin='round'
-                        d='m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z'
-                    />
-                </svg>
+            <div className='flex gap-4 items-center w-full max-w-lg'>
+                <div className='w-full max-w-lg flex items-center gap-3 border border-gray-300 rounded p-3 shadow-sm bg-white focus-within:ring-2 focus-within:ring-blue-500'>
+                    <svg
+                        xmlns='http://www.w3.org/2000/svg'
+                        fill='none'
+                        viewBox='0 0 24 24'
+                        strokeWidth={1.5}
+                        stroke='currentColor'
+                        className='w-6 h-6 text-gray-500'
+                    >
+                        <path
+                            strokeLinecap='round'
+                            strokeLinejoin='round'
+                            d='m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z'
+                        />
+                    </svg>
 
-                <input
-                    type='text'
-                    placeholder='Search by title, organization, qualification, etc...'
-                    className='w-full text-gray-700 placeholder-gray-400 border-none focus:outline-none bg-transparent'
-                    onChange={(e) => setFilters({ ...filters, title: e.target.value })}
-                />
+                    <input
+                        type='text'
+                        placeholder='Search by title, organization, qualification, etc...'
+                        className='w-full text-gray-700 placeholder-gray-400 border-none focus:outline-none bg-transparent'
+                        onChange={(e) => setFilters({ ...filters, title: e.target.value })}
+                    />
+                </div>
+                <div className=''>
+                <SignedOut>
+                    <SignInButton />
+                    <SignUpButton />
+                </SignedOut>
+                <SignedIn>
+                    <UserButton />
+                </SignedIn>
+                </div>
             </div>
         </div>
     )
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,12 @@ import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
 import { Analytics } from '@vercel/analytics/react';
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { SpeedInsights } from '@vercel/speed-insights/next';
+import Script from 'next/script';
+import Head from 'next/head';
+import {
+  ClerkProvider,
+} from '@clerk/nextjs';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -16,9 +21,9 @@ const geistMono = Geist_Mono({
 
 export const metadata: Metadata = {
   title:
-    'GovtJobAlerts | Latest Government Job Notifications, Results & Admit Cards',
+    'GovtJobsPortal | Latest Government Job Notifications, Results & Admit Cards',
   description:
-    'Stay updated with the latest government job notifications, application deadlines, admit cards, and results. GovtJobAlerts provides real-time updates on Sarkari Naukri, PSU jobs, railway jobs, bank jobs, and more. Apply now & never miss an opportunity!',
+    'Stay updated with the latest government job notifications, application deadlines, admit cards, and results. GovtJobsPortal provides real-time updates on Sarkari Naukri, PSU jobs, railway jobs, bank jobs, and more. Apply now & never miss an opportunity!',
 };
 
 export default function RootLayout({
@@ -27,17 +32,33 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang='en'>
-      <head>
-        <title>GovtJobPortal | Job Board</title>
-      </head>
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        <Analytics />
-        <SpeedInsights/>
-        {children}
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang='en'>
+        <Head>
+          <title>GovtJobsPortal | Job Board</title>
+          <link rel='preconnect' href='https://fonts.googleapis.com' />
+          <link
+            rel='stylesheet'
+            href='https://fonts.googleapis.com/css2?family=Inter:wght@400;600&family=Merriweather:wght@700&display=swap'
+          />
+        </Head>
+        <body
+          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        >
+          <Analytics />
+          <SpeedInsights />
+          {/* <header className='flex justify-end items-center p-4 gap-4 h-16'>
+            <SignedOut>
+              <SignInButton />
+              <SignUpButton />
+            </SignedOut>
+            <SignedIn>
+              <UserButton />
+            </SignedIn>
+          </header> */}
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
